04c9cee0e1ff8b32859ff6e32ef488c4767768ee
nyebula <ny79@njit.edu>
2024-07-21 16:37:39 -0700

    Started loginController and login router

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,2 +1,8 @@
 // Functions that are called by the request handled in the routes folder. Can use models in the models folder to interface with mongodb.
+const Account = require('..models/account')
 const mongoose = require('mongoose')
+
+
+const login = async (req, res) => {
+    
+}
--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,2 +1,8 @@
 // This will intercept the login requests and process them. It should send requests to their respective controllers.
 const express = require('express')
+const {
+    login
+} = require('../controllers/loginController')
+
+
+router.post('/login', login)
