0c34ffdc2411abc1ec8eaaea3ffc83503e3a7ebb
Kevin Ding <kevind888@gmail.com>
2024-07-21 19:03:51 -0400

    added initial account schema

--- backend/models/account.js
+++ backend/models/account.js
@@ -1,4 +1,19 @@
 // This will house the schema (structure) of a login/account of the database
 const mongoose = require('mongoose')
 
-const schema = mongoose.Schema
+const accountschema = new mongoose.Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true
+    }
+});
+
+const account = mongoose.model('Account', accountschema);
+
+module.exports = {account};
