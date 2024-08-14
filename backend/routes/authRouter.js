7f4d30a7f80e51e77d2fdc20469d3b4d241cb396
Kevin Ding <kevind888@gmail.com>
2024-08-14 01:14:04 -0400

    Changes to requirement of userVerification in authRouter

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,10 +1,10 @@
 // This will intercept the login requests and process them. It should send requests to their respective controllers.
 const express = require('express');
-const { userAuth } = require("../utils/helpers/userAuth");
+const { userVerification } = require("../utils/helpers/userAuth");
 const { home } = require('../controllers/homeController');
 const router = express.Router();
 
-router.use(userAuth)
+router.use(userVerification)
 router.post('/home', home)
 
 module.exports = router;
