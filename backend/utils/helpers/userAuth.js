4915a17c9b5617910859b4ec4bc0258fc944ef5c
Kevin Ding <kevind888@gmail.com>
2024-08-14 00:57:03 -0400

    Changes made to authUser, adding next parameter

--- backend/utils/helpers/userAuth.js
+++ backend/utils/helpers/userAuth.js
@@ -2,19 +2,22 @@ require("dotenv").config();
 const db = require("../../db/db");
 const jwt = require("jsonwebtoken");
 
-module.exports.userVerification = (req, res) => {
+module.exports.userVerification = (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
-    return res.status(401);;
+    res.status(401);
   }
-  jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
-    if (err) {
-      return res.status(401);
-    }
-    else {
-      // const user = db.fetchid(data.id)
-      // return res.status(200).json({ user: user.username })
-      return res.status(200)
-    }
-  })
+  else {
+    jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
+      if (err) {
+        res.status(401);
+      }
+      else {
+        // const user = db.fetchid(data.id);
+        // return res.status(200).json({ user: user.username });
+        res.status(200);
+      }
+    })  
+  }
+  next();
 }
